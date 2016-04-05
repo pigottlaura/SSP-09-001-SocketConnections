76fc309cf1c3fb0c63d66a37effa19fdd6a11065
Laura Pigott <pigottlaura@gmail.com>
2016-04-05 12:19:20 +0100

    Disabling default form action - logging out messages

--- public/javascripts/custom.js
+++ public/javascripts/custom.js
@@ -1,3 +1,18 @@
 jQuery(document).ready(function($){
     console.log("Page loaded");
+    
+    $("#chatForm").submit(function(){
+        if($("#myMessage").val() != ""){
+            console.log("Sending message - " + $("#myMessage").val());
+            $("#myMessage").val("");
+        } else {
+            console.log("There is no message to send");
+        }
+        
+        
+        
+        // Returning false, so the form can not complete it's default behaviour
+        // i.e. send post request
+        return false;
+    })
 });
